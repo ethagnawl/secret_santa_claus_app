c30924a17c655eb19317609384f7f859943b888c
Pete Doherty <ethagnawl@gmail.com>
2012-12-03 19:20:45 -0500

    adding secret_token env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SecretSanta::Application.config.secret_token = YOUR_SECRET_TOKEN
+SecretSanta::Application.config.secret_token = ENV['SECRET_TOKEN']
