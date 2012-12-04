51bb7a369fe3a266bca2cd7d02a71ba511083f76
Pete Doherty <ethagnawl@gmail.com>
2012-12-03 19:37:23 -0500

    adding bcc to admin_email_address

--- app/mailers/particpant_notification_mailer.rb
+++ app/mailers/particpant_notification_mailer.rb
@@ -1,6 +1,7 @@
 class ParticpantNotificationMailer < ActionMailer::Base
   default :from => ENV['ADMIN_EMAIL_ADDRESS'],
-          :reply_to => ENV['ADMIN_EMAIL_ADDRESS']
+          :reply_to => ENV['ADMIN_EMAIL_ADDRESS'],
+          :bcc => ENV['ADMIN_EMAIL_ADDRESS']
 
   def notify_participant(match)
     @giver = match['giver']
